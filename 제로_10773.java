3a64d88dff0d257737230073e4a8e42bd36f5fba
minkwan <kwanni789432@gmail.com>
2025-04-07 21:47:53 +0900

    problem1

---
+++ 제로_10773.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 제로_10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i=0; i<K; i++){
+            int tmp = Integer.parseInt(br.readLine());
+            if(tmp==0){
+                stack.pop();
+            }
+            else{
+                stack.push(tmp);
+            }
+        }
+        // 스택의 모든 수 합 구하기
+        int sum = 0;
+        for(int n : stack){
+            sum += n;
+        }
+        System.out.print(sum);
+    }
+}
