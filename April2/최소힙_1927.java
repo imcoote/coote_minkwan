190f384fafe085fbaeb95c90c45537d9a95ec5b5
minganin99 <mkmy789432@gmail.com>
2025-04-12 20:22:48 +0900

    modify

--- April2/최소힙_1927.java
+++ April2/최소힙_1927.java
@@ -16,16 +16,22 @@ import java.util.*;
 public class 최소힙_1927 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N =	Integer.parseInt(br.readLine());
-        PriorityQueue<Long> pq=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
 
         for(int i=0; i<N; i++) {
-            long x = Integer.parseInt(br.readLine());
-            if(x>0) {
-                pq.add(x);
-            }else if(x==0) {
-                if(pq.isEmpty()) System.out.println(0);
-                else System.out.println(pq.poll());
+            int x= Integer.parseInt(br.readLine());
+
+            if(x == 0) {
+                if(queue.isEmpty()) {
+                    System.out.println("0");
+                }
+                else {
+                    System.out.println(queue.poll());
+                }
+            }
+            else {
+                queue.add(x);
             }
         }
     }
