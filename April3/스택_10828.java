070b66f281c3b48baffd3cc4e2de241239415702
minganin99 <mkmy789432@gmail.com>
2025-04-14 22:32:21 +0900

    problem 1

---
+++ April3/스택_10828.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class 스택_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
