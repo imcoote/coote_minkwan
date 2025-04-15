204d4c07e4fea354f0685df7c06d3d29157a86cf
minganin99 <mkmy789432@gmail.com>
2025-04-15 22:29:02 +0900

    probelm 2

---
+++ April3/큐_10845.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class 큐_10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> queue = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    queue.offer(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        Integer last = null;
+                        for (Integer element : queue) {
+                            last = element;
+                        }
+                        System.out.println(last);
+                    }
+                    break;
+            }
+        }
+    }
+}
