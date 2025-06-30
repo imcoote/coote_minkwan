176f70e6828737719a6c85581785554cd80f912f
minganin99 <mkmy789432@gmail.com>
2025-06-30 09:58:38 +0900

    6월 4주차

--- June_4/TheReport.sql
+++ June_4/TheReport.sql
@@ -2,10 +2,10 @@ SELECT
     CASE 
         WHEN g.grade >= 8 THEN s.name 
         ELSE 'NULL'
-    END AS Name,---
+    END AS Name,
     g.grade AS Grade,
     s.marks AS Mark
-FROM students s JOIN grades g ON s.marks BETWEEN g.min_mark ANFD g.max_mark
+FROM students s JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
 ORDER BY
     g.grade DESC,
     CASE WHEN g.grade >= 8 THEN s.name END ASC,
